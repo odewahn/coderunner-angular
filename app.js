c72990f9c2bd54473fad22f9ba92a9cf5bd31236
Andrew Odewahn <odewahn@oreilly.com>
2014-07-02 12:25:40 -0400

    working

--- app.js
+++ app.js
@@ -3,10 +3,27 @@
    var app = angular.module('console', []);
 
    app.controller('CoderunnerControl', function() {
+	
+	  // Set up some default values
 	  this.supported_languages=["ruby", "python" , "lua", "javascript", "coffeescript"];
 	  this.selected_language = "ruby"
+
+      var handle = this;
+
       this.mode = "editor"
       this.control_button_label = "Run"
+
+
+      // Toggle the mode between code view and output view
+      this.toggleMode = function() {
+	     if (handle.mode === "editor") {
+            handle.mode = "output";
+            handle.control_button_label = "Editor"
+	     } else {
+            handle.mode = "editor";
+            handle.control_button_label = "Run"		
+	     }
+      }
 
       //I'd start the JSREPL right in here -- it'll be great!
 
